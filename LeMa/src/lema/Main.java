d15796527a9cedfb572a8d5463f5387276d79b58
Yuli <Yuli@192.168.1.42>
2015-06-12 01:53:11 -0500

    Analizador Lexico finalizado, conexion con analizador sintactico finalizado

--- LeMa/src/lema/Main.java
+++ LeMa/src/lema/Main.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java_cup.runtime.Symbol;
-
+/* COMPILADOR */
 public class Main {
 
     public static void main(String[] args) throws IOException
